06bfc98055b5da6fe78e08622e3f7a6749db5d84
Rigo Villanueva <rigovillanueva@rename-me.local>
2025-05-19 12:25:45 -0400

    implement sign up logic and page

--- utp_capstone/urls.py
+++ utp_capstone/urls.py
@@ -21,7 +21,8 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("pages.urls")),
+    path("pages/", include("pages.urls")),
+    path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
 
